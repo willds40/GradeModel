3242f434113b84644b7f490a2d066fa691f2898b
wdsand <wdsand@thoughtworks.com>
2018-01-19 12:47:17 -0500

    makes comment box its own component

--- src/components/comments.js
+++ src/components/comments.js
@@ -1,15 +1,10 @@
 import React, { Component } from 'react';
+import CommentBox from './comment-box'
 export default class Comments extends Component {
     render() {
         return (
             <div className='comments col-md-4'>
-            <div id="container">
-              <input className="input-comment"/>
-                <select className=" selectpicker select-comment">
-                   <option value="Value for Item 1" title="Title for Item 1">er companions instrument set estimating sex remarkably solicitude motionless. Property men the why smallest graceful day insisted required. Inquiry justice country old placing sitting any ten age.</option>
-                  <option value="Value for Item 3" title="Title for Item 3">Item 3</option>
-                </select>
-            </div>
+              <CommentBox />
             </div>
         );
     }
