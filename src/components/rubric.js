cea547d27509cbbcea1b46ca71f379fe84d22a76
wdsand <wdsand@thoughtworks.com>
2018-01-22 15:44:54 -0500

    makes rubric sticky

--- src/components/rubric.js
+++ src/components/rubric.js
@@ -1,27 +1,32 @@
 import React, { Component } from 'react';
+import Sticky from 'react-stickynode';
 export default class Rubric extends Component {
     render() {
         return (
-          <table className="col-md-12 rubric">
-          <tr>
-          <td className='rubric-title'>THESIS</td>
-          <td className='rubric-title rubric-section'>EVIDENCE</td>
-          <td className='rubric-title rubric-section'>ANALYSIS</td>
-          <td className='rubric-title rubric-section'>CONCLUSION</td>
-          </tr>
-          <tr>
-          <td> Rubric information</td>
-          <td className='rubric-section'> Rubric  on information</td>
-          <td className='rubric-section'> Rubric information</td>
-          <td className = 'rubric-section'> Rubric information </td>
-          </tr>
-          <tr>
-          <td className='rubric-number '> 4</td>
-          <td className='rubric-number rubric-section'> 3</td>
-          <td className='rubric-number rubric-section'>4</td>
-          <td className='rubric-number rubric-section'>3</td>
-          </tr>
-          </table>
+          <div className='rubric-container'>
+          <Sticky enabled={true} top={0}>
+            <table className="col-md-12 rubric">
+            <tr>
+            <td className='rubric-title'>THESIS</td>
+            <td className='rubric-title rubric-section'>EVIDENCE</td>
+            <td className='rubric-title rubric-section'>ANALYSIS</td>
+            <td className='rubric-title rubric-section'>CONCLUSION</td>
+            </tr>
+            <tr>
+            <td> Rubric information</td>
+            <td className='rubric-section'> Rubric  on information</td>
+            <td className='rubric-section'> Rubric information</td>
+            <td className = 'rubric-section'> Rubric information </td>
+            </tr>
+            <tr>
+            <td className='rubric-number '> 4</td>
+            <td className='rubric-number rubric-section'> 3</td>
+            <td className='rubric-number rubric-section'>4</td>
+            <td className='rubric-number rubric-section'>3</td>
+            </tr>
+            </table>
+          </Sticky>
+          </div>
         );
     }
 }
