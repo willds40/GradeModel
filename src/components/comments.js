63ef2f20cec2132da97f6ab843b4f807f615571b
wdsand <wdsand@thoughtworks.com>
2018-01-24 13:10:13 -0500

    adds image to comment box

--- src/components/comments.js
+++ src/components/comments.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import CommentBox from './comment-box'
+import plusIcon from '../assets/icons/plus-icon.png'
 export default class Comments extends Component {
     render() {
         return (
@@ -10,6 +11,7 @@ export default class Comments extends Component {
               <CommentBox />
               <CommentBox />
               <CommentBox />
+              <img src={plusIcon} />
             </div>
         );
     }
--- webpack.config.js
+++ webpack.config.js
@@ -8,13 +8,22 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
-    loaders: [{
+     loaders: [
+       {
       exclude: /node_modules/,
       loader: 'babel',
       query: {
         presets: ['react', 'es2015', 'stage-1']
       }
-    }]
+    },
+    {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+          'file?hash=sha512&digest=hex&name=[hash].[ext]',
+          'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
+      }
+    ]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
