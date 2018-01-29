2ab3b0fa48f0c0f72762e6e2b11ce9a837ef4ad6
wdsand <wdsand@thoughtworks.com>
2018-01-29 11:11:13 -0500

    adds close icon to comment box

--- src/components/comment-box.js
+++ src/components/comment-box.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
+import closeIcon from '../assets/icons/close-icon.png'
 
 export default class CommentBox extends Component {
     render() {
         return (
           <div className='input-comment-container'>
-            <input className="input-comment"/>
-              <select className=" selectpicker select-comment">
-                 <option value="Value for Item 1" title="Title for Item 1">er companions instrument set estimating sex remarkably solicitude motionless. Property men the why smallest graceful day insisted required. Inquiry justice country old placing sitting any ten age.</option>
-                <option value="Value for Item 3" title="Title for Item 3">Item 3</option>
-              </select>
+          <img src={closeIcon} className='close-comment-icon' />
+          <input className="input-comment"/>
+            <select className=" selectpicker select-comment">
+               <option value="Value for Item 1" title="Title for Item 1">er companions instrument set estimating sex remarkably solicitude motionless. Property men the why smallest graceful day insisted required. Inquiry justice country old placing sitting any ten age.</option>
+              <option value="Value for Item 3" title="Title for Item 3">Item 3</option>
+            </select>
           </div>
         );
     }
