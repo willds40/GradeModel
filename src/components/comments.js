29d1f489a00425d19296a4bc1221978f05d446c9
wdsand <wdsand@thoughtworks.com>
2018-01-29 11:23:47 -0500

    centers the add comment button

--- src/components/comments.js
+++ src/components/comments.js
@@ -11,7 +11,7 @@ export default class Comments extends Component {
               <CommentBox />
               <CommentBox />
               <CommentBox />
-              <span className='add-comment'>
+              <span className='add-comment col-md-8 '>
               <img src={plusIcon} className='add-comment-icon' />
               <label>Add Comment</label>
               </span>
