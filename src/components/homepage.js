59a8d3063def15bf01b25bca1c89b4422b3cf8be
wdsand <wdsand@thoughtworks.com>
2018-01-31 10:18:51 -0500

    adds summmary section

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import Essay from "./essay";
 import Rubric from './rubric';
 import Comments from './comments';
+import Summary from './summary';
 
 export default class Homepage extends Component {
     render() {
         return (
           <div>
               <Rubric />
+              <Summary />
               <Essay />
               <Comments />
           </div>
