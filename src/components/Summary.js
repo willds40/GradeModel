4c6eda84a4d4d569d3b1022c6e0fe82935ded656
wdsand <wdsand@thoughtworks.com>
2018-02-07 17:49:43 -0500

    adds summary and updates jquery

---
+++ src/components/Summary.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+export default class Summary extends Component {
+    render() {
+        return (
+          <div className='summary'>
+            <h2 className='summary-title'>SUMMARY</h2>
+          <div>
+                <ul>
+                  <li>Do in laughter securing smallest sensible no mr hastened.Do in laughter securing smallest sensible no mr</li>
+                  <li>perhaps proceed in in brandon of limited unknown greatly. Do in laughter securing smallest sensible no mr hastened.</li>  <li>Distrusts fulfilled happiness unwilling as explained of difficult.Do in laughter securing smallest sensible no mr hastened.</li>
+                  <li>No landlord of peculiar ladyship attended if contempt ecstatic.Do in laughter securing smallest sensible no mr hastened. </li>
+                </ul>
+          </div>
+          </div>
+
+        );
+    }
+}
