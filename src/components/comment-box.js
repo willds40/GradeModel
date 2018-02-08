a3946c1a6520b2a61e46f492ebd640a8b964a096
wdsand <wdsand@thoughtworks.com>
2018-02-08 18:59:56 -0500

    selected comment populate dropdonw

--- src/components/comment-box.js
+++ src/components/comment-box.js
@@ -2,14 +2,22 @@ import React, { Component } from 'react';
 import closeIcon from '../assets/icons/close-icon.png'
 
 export default class CommentBox extends Component {
+  constructor(props){
+    super(props);
+    this.state = { value:""};
+  }
+  handleChange(event){
+    this.setState({value:event.target.options[event.target.selectedIndex].text})
+  }
     render() {
         return (
           <div className='input-comment-container'>
           <img src={closeIcon} className='close-comment-icon' />
-          <input className="input-comment"/>
-            <select className=" selectpicker select-comment">
-               <option value="Value for Item 1" title="Title for Item 1">er companions instrument set estimating sex remarkably solicitude motionless. Property men the why smallest graceful day insisted required. Inquiry justice country old placing sitting any ten age.</option>
-              <option value="Value for Item 3" title="Title for Item 3">Item 3</option>
+          <input value={this.state.value} className="input-comment"/>
+            <select onChange={this.handleChange.bind(this)} className=" selectpicker select-comment">
+             <option>Select</option>
+             <option>er companions instrument set estimating sex remarkably solicitude motionless. Property men the why smallest graceful day insisted required. Inquiry justice country old placing sitting any ten age.</option>
+            <option>Item 3</option>
             </select>
           </div>
         );
