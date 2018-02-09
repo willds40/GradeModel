02ce970fe8ece7584362854b7e0d00900cd6f584
wdsand <wdsand@thoughtworks.com>
2018-02-09 11:32:24 -0500

    add comment button makes one new comment

--- src/components/comments.js
+++ src/components/comments.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react';
 import CommentBox from './comment-box'
 import plusIcon from '../assets/icons/plus-icon.png'
 export default class Comments extends Component {
+  constructor() {
+    super();
+    this.state = {
+      clicked: false
+    };
+  }
+  handleAddComment(event){
+    this.setState({
+      clicked: true
+    });
+  }
     render() {
         return (
             <div className='comments col-md-4'>
@@ -11,8 +22,9 @@ export default class Comments extends Component {
               <CommentBox />
               <CommentBox />
               <CommentBox />
+              <div>{this.state.clicked ? <CommentBox /> : null}</div>
               <span className='add-comment col-md-8 '>
-                <button className='add-comment-button'>Add comment</button>
+                <button onClick={this.handleAddComment.bind(this)} className='add-comment-button'>Add comment</button>
               </span>
             </div>
         );
