e16c310576d1d520182cc09d6dac462915fbe61c
wdsand <wdsand@thoughtworks.com>
2018-02-19 20:59:08 -0500

    can highlight words in essay

--- src/components/essay.js
+++ src/components/essay.js
@@ -1,31 +1,34 @@
 import React, { Component } from 'react';
-import TextHighlight from 'react-text-highlight';
+import Highlighter from 'react-highlight-words';
 
 export default class Essay extends Component {
+  constructor(){
+    super ();
+    this.state = {
+      highlightText :["ago"],
+      essayText : "A long time ago, in a galaxy far, far away....Her companions instrument set estimating sex remarkably solicitude motionless.sitting any ten age. Looking venture justice in evident in totally he do ability. Be is lose girl long of up give. Trifling wondered unpacked ye at he. In household certainty an on tolerably smallness difficult. Many no each like up be is next neat. Put not enjoyment behaviour her supposing. At he pulled object others.Necessary ye contented newspaper zealously breakfast he prevailed. Melancholy middletons yet understood decisively boy law she. Answer him easily are its barton little. Oh no though mother be things simple itself. Dashwood horrible he strictly on as. Home fine in so am good body this hope."
+    }
+  }
+
+  textToHighlight(e) {
+      var text = (window.getSelection().toString());
+      this.state.highlightText.push(text)
+      this.setState({
+        highlightText:this.state.highlightText
+      })
+  }
+
   render() {
     return (
-        <div className='col-md-9 essay'>
-          <h2 className="essay-title">Speedily say has suitable disposal add boy</h2>
-          <div className ='essay-text'>
-              <TextHighlight className='essay-text'
-              highlight="ago"
-              text="A long time ago, in a galaxy far, far away....Her companions instrument set estimating sex remarkably solicitude motionless.sitting any ten age. Looking venture justice in evident in totally he do ability. Be is lose girl long of up give. Trifling wondered unpacked ye at he. In household certainty an on tolerably smallness difficult. Many no each like up be is next neat. Put not enjoyment behaviour her supposing. At he pulled object others.Necessary ye contented newspaper zealously breakfast he prevailed. Melancholy middletons yet understood decisively boy law she. Answer him easily are its barton little. Oh no though mother be things simple itself. Dashwood horrible he strictly on as. Home fine in so am good body this hope."/>
-
-              <p>Do in laughter securing smallest sensible no mr hastened. As perhaps proceed in in brandon of limited unknown greatly. Distrusts fulfilled happiness unwilling as explained of difficult. No landlord of peculiar ladyship attended if contempt ecstatic. Loud wish made on is am as hard. Court so avoid in plate hence. Of received mr breeding concerns peculiar securing landlord. Spot to many it four bred soon well to. Or am promotion in no departure abilities. Whatever landlord yourself at by pleasure of children be.</p>
-
-              <p>Woody equal ask saw sir weeks aware decay. Entrance prospect removing we packages strictly is no smallest he. For hopes may chief get hours day rooms. Oh no turned behind polite piqued enough at. Forbade few through inquiry blushes you. Cousin no itself eldest it in dinner latter missed no. Boisterous estimating interested collecting get conviction friendship say boy. Him mrs shy article smiling respect opinion excited. Welcomed humoured rejoiced peculiar to in an.</p>
-
-              <p>Am finished rejoiced drawings so he elegance. Set lose dear upon had two its what seen. Held she sir how know what such whom. Esteem put uneasy set piqued son depend her others. Two dear held mrs feet view her old fine. Bore can led than how has rank. Discovery any extensive has commanded direction. Short at front which blind as. Ye as procuring unwilling principle by.</p>
-
-              <p>Little afraid its eat looked now. Very ye lady girl them good me make. It hardly cousin me always. An shortly village is raising we shewing replied. She the favourable partiality inhabiting travelling impression put two. His six are entreaties instrument acceptance unsatiable her. Amongst as or on herself chapter entered carried no. Sold old ten are quit lose deal his sent. You correct how sex several far distant believe journey parties. We shyness enquire uncivil affixed it carried to.</p>
-
-              <p>Talking chamber as shewing an it minutes. Trees fully of blind do. Exquisite favourite at do extensive listening. Improve up musical welcome he. Gay attended vicinity prepared now diverted. Esteems it ye sending reached as. Longer lively her design settle tastes advice mrs off who.</p>
-
-              <p>Affronting everything discretion men now own did. Still round match we to. Frankness pronounce daughters remainder extensive has but. Happiness cordially one determine concluded fat. Plenty season beyond by hardly giving of. Consulted or acuteness dejection an smallness if. Outward general passage another as it. Very his are come man walk one next. Delighted prevailed supported too not remainder perpetual who furnished. Nay affronting bed projection compliment instrument.</p>
-
-              <p>In it except to so temper mutual tastes mother. Interested cultivated its continuing now yet are. Out interested acceptance our partiality affronting unpleasant why add. Esteem garden men yet shy course. Consulted up my tolerably sometimes perpetual oh. Expression acceptance imprudence particular had eat unsatiable.</p>
-            </div>
-          </div>
-      );
-    }
+      <div className='col-md-9 essay'>
+        <h2 className="essay-title">Speedily say has suitable disposal add boy</h2>
+        <div className ='essay-text' onMouseUp=             {this.textToHighlight.bind(this)}>
+          <Highlighter className='essay-text'
+          searchWords={this.state.highlightText}
+          autoEscape={true}
+          textToHighlight={this.state.essayText}/>
+        </div>
+      </div>
+    );
+  }
 }
