80e169b6d8a32c0b812b3f5b6a8c334dbce1ab7c
wdsand <wdsand@thoughtworks.com>
2018-05-03 19:09:26 -0400

    refactos homepage

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -13,26 +13,16 @@ export default class Homepage extends Component {
     commmentTypeBar:false,
     commentType:"",
     }
+    this.onToggleSummary = this.onToggleSummary.bind(this)
+    this.onSubmitGrade = this.onSubmitGrade.bind(this)
   }
   onSubmitGrade(){
     window.alert("Thank You For Submitting Your Grade");
   }
 
-  onOpenSummary = () => {
-    this.setState({ open: true });
+  onToggleSummary = () => {
+    this.setState({ open: !this.state.open });
   };
-
-  onCloseSummary = () => {
-    this.setState({
-    open: false,
-    });
-  }
-
-  onCommentSelect(e){
-    this.setState({
-      commentType :e.target.value
-    })
-  }
 
   render() {
     return (
@@ -40,22 +30,19 @@ export default class Homepage extends Component {
         <Rubric />
         {this.state.open ===  false ?
           <span className='add-comment col-md-7 '>
-            <button onClick={this.onOpenSummary.bind(this)} className='summary-button'>View Summary</button>
-          </span> : <span className='add-comment col-md-7 '>
-            <button onClick={this.onCloseSummary.bind(this)} className='summary-button'>Close Summary</button>
+            <button onClick={this.onToggleSummary} className='summary-button'>View Summary</button>
+          </span> :
+          <span className='add-comment col-md-7 '>
+            <button onClick={this.onToggleSummary} className='summary-button'>Close Summary</button>
           </span>
         }
         {this.state.open ===  false ? null :
           <Summary />
         }
-        <Essay
-        commentType = {this.state.commentType}
-        />
-        <Comments
-        onCommentSelect={this.onCommentSelect.bind(this)}
-        />
+        <Essay />
+        <Comments/>
         <div className='submit-essay col-md-8'>
-          <button onClick={this.onSubmitGrade.bind(this)} className='submit-button'>Submit Grade</button>
+          <button onClick={this.onSubmitGrade} className='submit-button'>Submit Grade</button>
         </div>
       </div>
     );
