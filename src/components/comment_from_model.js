c7efcc75507e588a1339aa53087f8fcf96f55186
wdsand <wdsand@thoughtworks.com>
2018-05-06 19:07:51 -0400

    creates comment from model component

---
+++ src/components/comment_from_model.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import CommentBox from './comment-box'
+
+export default class CommentFomModel extends Component{
+  constructor(){
+    super()
+  }
+
+  render(){
+     return (
+        <div className= {`input-comment-container ${this.props.commentType}-section`}>
+          <button  className="delete-comment-button"
+          onClick= {this.props.handleDeleteComment}> Remove
+          </button>
+          <h4 className='comment-type'>{this.props.commentType}</h4>
+          <CommentBox
+          value={this.props.commentValue}
+          />
+        </div>
+      );
+  }
+}
